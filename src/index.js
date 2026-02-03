0f3665306cc10679a33d86703a6c4fc20970cccc
Swan <swanand556@gmail.com>
2026-02-03 14:01:06 +0530

    header, body and footer created

--- src/index.js
+++ src/index.js
@@ -49,8 +49,9 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!!</h1>
-      <Pizza />
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
@@ -62,6 +63,26 @@ function Pizza() {
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menus</h2>
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!!</footer>
   );
 }
 
